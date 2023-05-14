05d889bd673cfbe4b96f1f246b2098a9cf29582f
dmitry yunkov <dima.yonkov@gmail.com>
2023-05-14 18:12:09 +0300

    semen logic

--- src/models/Fighter.ts
+++ src/models/Fighter.ts
@@ -4,28 +4,54 @@ export class Fighter {
   public fights = 0;
   public wins = 0;
   public looses = 0;
+  private resultStatuses = ['win', 'lose'];
 
   constructor(public id: number, public name: string) {}
 
   public fight(enemy: Fighter): { winner: Fighter; looser: Fighter } {
-    const rand = Math.floor(Math.random() * 2);
+    const rand = this.resultStatuses[Math.floor(Math.random() * 2)];
 
-    if (rand === 0) {
-      this.scores += 10;
-      enemy.scores -= 10;
-
-      return {
-        winner: this,
-        looser: enemy,
-      };
-    } else {
-      this.scores -= 10;
-      enemy.scores += 10;
-
-      return {
-        winner: enemy,
-        looser: this,
-      };
+    switch (rand) {
+      case 'win':
+        return this.getWinner(this, enemy);
+      case 'lose':
+        return this.getWinner(enemy, this);
     }
+  }
+
+  private getScoreResult(scoreWin: number, scoreLose: number): { winnerScore: number; looserScore: number } {
+    let finalWinScore = Math.floor(scoreLose * 0.1);
+    let finalLoseScore = Math.floor(scoreWin * 0.1);
+
+    if ((finalLoseScore / finalWinScore) * 100 < 80) {
+      finalWinScore = Math.floor(scoreLose * 0.05);
+      finalLoseScore = Math.floor(scoreWin * 0.15);
+    }
+
+    if ((finalWinScore / finalLoseScore) * 100 < 80) {
+      finalWinScore = Math.floor(scoreLose * 0.15);
+      finalLoseScore = Math.floor(scoreWin * 0.05);
+    }
+
+    // lucky win
+    const isLuckyWin = 1 === Math.floor(Math.random() * 10);
+
+    if (isLuckyWin) {
+      finalWinScore = Math.floor(scoreLose * 0.25);
+      finalLoseScore = Math.floor(scoreWin * 0.25);
+    }
+
+    return { winnerScore: finalWinScore, looserScore: finalLoseScore };
+  }
+
+  private getWinner(winner: Fighter, loser: Fighter): { winner: Fighter; looser: Fighter } {
+    const resultScore = this.getScoreResult(winner.scores, loser.scores);
+    winner.scores -= resultScore.winnerScore;
+    loser.scores += resultScore.looserScore;
+
+    return {
+      winner: winner,
+      looser: loser,
+    };
   }
 }
